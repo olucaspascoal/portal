bb67b52bed47daec17fe062985e4130b85f8f6a6
Lucas Pascoal <olucaspascoal@gmail.com>
2022-01-18 00:22:04 -0300

    Criação do router para páginas de 'Início', 'Quem somos' e 'Atendimento'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AtendimentoComponent } from './atendimento/atendimento.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'atendimento', component: AtendimentoComponent},
+  {path: 'quem-somos', component: QuemSomosComponent},
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { PlanosComponent } from './planos/planos.component';
+import { AtendimentoComponent } from './atendimento/atendimento.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SobreComponent,
+    InicioComponent,
+    PlanosComponent,
+    AtendimentoComponent,
+    QuemSomosComponent
   ],
   imports: [
     BrowserModule,
